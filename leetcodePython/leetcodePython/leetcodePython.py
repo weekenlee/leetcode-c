300e1f66572ef787d6861540703cf5147f6d55d5
lwj1396@163.com <lwj1396@163.com>
2018-01-15 10:53:23 +0800

    twosum py

---
+++ leetcodePython/leetcodePython/leetcodePython.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup = {}
+        for i, num in enumerate(nums):
+            if target - num in lookup:
+                return [lookup[target - num], i]
+            lookup[num] = i
+
+
+
+if __name__ == '__main__':
+    print(Solution().twoSum((2, 7, 11, 15), 9))
