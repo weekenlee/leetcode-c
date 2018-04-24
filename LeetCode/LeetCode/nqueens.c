a47d20daa720d2b1a8bdc835a42790ae8398e60b
lwj1396@163.com <lwj1396@163.com>
2018-04-24 18:36:51 +0800

    51

--- LeetCode/LeetCode/nqueens.c
+++ LeetCode/LeetCode/nqueens.c
@@ -36,7 +36,7 @@ static inline int top(int *stack, int n)
 	return 0;
 }
 
-static char **solutions(int *stack, int n)
+static char **solution(int *stack, int n)
 {
 	int row, col;
 	char **solution = malloc(n*sizeof(char *));
@@ -54,6 +54,77 @@ static char **solutions(int *stack, int n)
 
 }
 
-char** solveNQueens(int n , int *returnSize) {
 
+char** solveNQueens(int n , int *returnSize) 
+{
+	int row = 0, col = 0, sum = 0;
+	char ***solutions = malloc(1000 * sizeof(char **));
+
+	int *stack = malloc(n * sizeof(int));
+	for (row = 0; row < n; ++row)
+	{
+		stack[row] = -1;
+	}
+
+	if(n == 1) {
+		stack[0] =0;
+		solutions[0] = solution(stack, n);
+		*returnSize = 1;
+		return solutions;
+	}
+
+	for(;;) {
+		for(; row < n; row++) {
+			while(col < n) {
+				if(conflict(stack, row, col)) {
+					while(col == n -1 ) {
+						if(--row < 0) {
+							free(stack);
+							*returnSize = sum;
+							return solutions;
+						}
+						col = pop(stack, row);
+					}
+					col++;
+				}else {
+					push(stack, row, col);
+					break;
+				}
+			}
+			col = 0;
+		}
+		row = top(stack, n);
+		if(row == n - 1) {
+			solutions[sum++] = solution(stack,n);
+		}
+
+		col = pop(stack, row);
+		col++;
+	}
+	assert(0);
+}
+
+int main(int argc, char** argv)
+{
+	int i, n, row, col, num_of_solution;
+	if(argc != 2) {
+		printf("Usage: ./queen 8\n");
+		exit(-1);
+	}
+
+	n = atoi(argv[1]);
+	char ***solutions = solveNQueens(n, &num_of_solution);
+	for (i = 0; i < num_of_solution; i++) {
+        char **solution = solutions[i];
+        for (row = 0; row < n; row++) {
+            char *line = solution[row];
+            for (col = 0; col < n; col++) {
+                putchar(line[col]);
+            }
+            putchar('\n');
+        }
+        printf("The %dth solution.\n", i + 1);
+    }
+
+	return 0;
 }
