d6372aec245b40dabc61026a90b72a28fa87a71a
lwj1396@163.com <lwj1396@163.com>
2018-05-21 14:26:36 +0800

    61

--- LeetCode/LeetCode/rotate_list.c
+++ LeetCode/LeetCode/rotate_list.c
@@ -6,7 +6,7 @@ struct ListNode {
     struct ListNode *next;
 };
 
-static struct ListNode* rotateRight() {
+static struct ListNode* rotateRight(struct ListNode *head, int k) {
     if(head == NULL || k <= 0) {
         return head;
     }
@@ -20,8 +20,54 @@ static struct ListNode* rotateRight() {
     while(k>0) {
         if(p == NULL){
             int length = count - k;
+            prev = &dummy;
+            p = head;
+            k = count % length;
+            if(k == 0) break;
         }
+        prev = p;
+        p = p->next;
+        k--;
     }
     
+    while(p != NULL) {
+        last = last->next;
+        prev = p;
+        p = p->next;
+    }
+
+    if(last != &dummy){
+        prev->next = head;
+        dummy.next = last->next;
+        last->next = NULL;
+    }
+
+    return dummy.next;
+}
+
+int main(int argc, char **argv){
+    int i;
+    struct ListNode *p, *prev, dummy, *list;
+    
+    dummy.next = NULL;
+    prev = &dummy;
+    for (i = 2; i < argc; i++) {
+        p = malloc(sizeof(*p));
+        int n = atoi(argv[i]);
+        printf("%d ", n);
+        p->val = n;
+        p->next = NULL;
+        prev->next = p;
+        prev = p;
+    }
+    putchar('\n');
+
+    list = rotateRight(dummy.next, atoi(argv[1]));
+    for (p = list; p != NULL; p = p->next) {
+        printf("%d ", p->val);
+    }
+    putchar('\n');
+
+    return 0;
 }
 
