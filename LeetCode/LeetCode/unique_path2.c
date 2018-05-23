3361719d2c4a1536eb6a3ad3364d0aae2811dd11
lwj1396@163.com <lwj1396@163.com>
2018-05-23 09:54:23 +0800

    63

--- LeetCode/LeetCode/unique_path2.c
+++ LeetCode/LeetCode/unique_path2.c
@@ -7,7 +7,7 @@ static int uniquePathsWithObstacles(int **obstacleGrid,
 {
     int row, col;
     int reset = 0;
-    for(rpw = 0; row < obstacleGridRowSize; row++) {
+    for(row = 0; row < obstacleGridRowSize; row++) {
         if(reset){
             obstacleGrid[row][0] = 1;
         }else {
@@ -30,8 +30,38 @@ static int uniquePathsWithObstacles(int **obstacleGrid,
 
     for(row = 0; row < obstacleGridRowSize; row++) {
         int *line = obstacleGrid[row];
-        for(col = 0; col < obstacleGridColSize; col ++) }{
+        for(col = 0; col < obstacleGridColSize; col ++) {
             line[col] ^= 1;
         }
     }
+
+    for(row = 1; row < obstacleGridRowSize; row++) {
+        int *last_line = obstacleGrid[row - 1];
+        int *line = obstacleGrid[row];
+        for(col = 1; col < obstacleGridColSize; col++) {
+            if(line[col] != 0) {
+                line[col] = line[col - 1] + last_line[col];
+            }
+        }
+    }
+
+    return obstacleGrid[obstacleGridRowSize - 1][obstacleGridColSize - 1];
+}
+
+int main(int argc, char **argv) {
+    int i, j, k = 3;
+    int row_size = atoi(argv[1]);
+    int col_size = atoi(argv[2]);
+    int **grids = malloc(row_size * sizeof(int *));
+    for (i = 0; i < row_size; i++) {
+        grids[i] = malloc(col_size * sizeof(int));
+        int *line = grids[i];
+        for (j = 0; j < col_size; j++) {
+            line[j] = atoi(argv[k++]);
+            printf("%d ", line[j]);
+        }
+        printf("\n");
+    }
+    printf("%d\n", uniquePathsWithObstacles(grids, row_size, col_size));
+    return 0;
 }
