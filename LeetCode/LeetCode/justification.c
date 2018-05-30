7ee08b07761cef741b4cc17f8e6e6b99b9a40c8e
lwj1396@163.com <lwj1396@163.com>
2018-05-30 13:13:55 +0800

    68

--- LeetCode/LeetCode/justification.c
+++ LeetCode/LeetCode/justification.c
@@ -36,11 +36,84 @@ line_fill(char *line,
 
 static char**
 fullJustify(char** words,
-            int wordSize,
+            int wordsSize,
             int maxWidth,
             int *returnSize)
 {
     int i, j, k, cap = 100, count = 0;
     char **lines = malloc(cap *sizeof(char *));
     char *buf = malloc(cap *(maxWidth + 1));
+
+    for(i = 0; i < cap; i++) {
+        lines[i] = buf + i * (maxWidth +1);
+    }
+
+    int *word_lens = malloc(wordsSize * sizeof(int));
+    for(i = 0; i<wordsSize; i++) {
+        word_lens[i] = strlen(words[i]);
+    }
+
+    int wc = 0;
+    int len = 0;
+    int start = 0;
+    int chars= 0;
+    for(i = 0, j = 0; i < wordsSize; i++) {
+        if(len + word_lens[i] > maxWidth) {
+            int even_spaces = wc == 1 ? 0 : (maxWidth - chars) / (wc - 1);
+            int remain_spaces = wc == 1 ? 0 : (maxWidth - chars) %(wc - 1);
+            line_fill(lines[count], len, words, word_lens, maxWidth,
+                    even_spaces, remain_spaces, start, i);
+            count++;
+            wc = 1;
+            len = word_lens[i] + 1;
+            chars = word_lens[i];
+            start = i;
+        }else if(len + word_lens[i] == maxWidth) {
+            chars += word_lens[i];
+            int even_spaces = wc == 0 ? 0 : (maxWidth - chars)/wc;
+            int remain_spaces = wc == 0 ? 0 :(maxWidth - chars)%wc;
+            line_fill(lines[count], len, words, word_lens, maxWidth,
+                    even_spaces, remain_spaces, start, i + 1);
+            count++;
+            wc = 0;
+            len = 0;
+            chars = 0;
+            start = i + 1;
+        }else {
+            chars += word_lens[i];
+            len += word_lens[i] + 1;
+            wc++;
+        }
+    }
+
+    if(wc > 0){
+        char *p = lines[count];
+        for(i = start; i< start + wc; i++) {
+            memcpy(p, words[i], word_lens[i]);
+            p += word_lens[i];
+            if(i < start + wc - 1) {
+                *p++ = ' ';
+            }
+        }
+        while( p - lines[count] < maxWidth) {
+            *p++ = ' ';
+        }
+        *p++ = '\0';
+        count++;
+    }
+
+    *returnSize = count;
+    return lines;
+}
+
+int main(int argc, char** argv)
+{
+    int i, count;
+
+    char** lines = fullJustify(argv + 2, argc - 2, atoi(argv[1]), &count);
+    for (i = 0; i < count; i++) {
+        printf("%s\n", lines[i]);
+    }
+
+    return 0;
 }
