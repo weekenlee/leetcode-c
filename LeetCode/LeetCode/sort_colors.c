2cace07474785cef62a6cbf021cce112ab234b81
lwj1396@163.com <lwj1396@163.com>
2018-06-09 12:21:16 +0800

    785

--- LeetCode/LeetCode/sort_colors.c
+++ LeetCode/LeetCode/sort_colors.c
@@ -17,9 +17,38 @@ static void sortColors(int *nums, int numsSize)
             i++;
         }
 
-        while(nums[j] ==0 && j > i) {
+        while(nums[j] !=0 && j > i) {
             j--;
         }
-        swap()
+        swap(nums + i, nums + j);
     }
+    j = numsSize - 1;
+    while(i<j){
+        while(nums[i] == 1 && i < j){
+            i++;
+        }
+        while(nums[j] != 1 && j > i) {
+            j--;
+        }
+        swap(nums + i, nums+j);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2) {
+        fprintf(stderr, "Usage: ./test colors\n");
+        exit(-1);
+    }
+    int i, count = strlen(argv[1]);
+    int *nums = malloc(count * sizeof(int));
+    for (i = 0; i < count; i++) {
+        nums[i] = argv[1][i] - '0';
+    }
+    sortColors(nums, count);
+    for (i = 0; i < count; i++) {
+        printf("%d", nums[i]);
+    }
+    printf("\n");
+    return 0;
 }
