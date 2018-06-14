f569fa379107b5e3acd878b64a3f0ac8e265807a
lwj1396 <lwj1396@163.com>
2018-06-14 11:30:56 +0800

    79

--- LeetCode/LeetCode/word_search.c
+++ LeetCode/LeetCode/word_search.c
@@ -20,6 +20,21 @@ dfs(char **word, char **board,  bool *used
     bool result = false;
 
     if(row > 0&& ！used[(row - 1)*col_size + col]) {
-        result = dfs(word+1, board, used, row - 1, col, row_size, col_size);
+        result = dfs(word+1, board, used, row-1, col, row_size, col_size);
     }
+
+    if(!result && row < row_size && !used[(row + 1) * col_size + col]) {
+        result = dfs(word+1, board, used, row+1, col, row_size, col_size);
+    }
+
+    if(!result && col > 0&& !used[row*col_size + col -1]) {
+        result = dfs(word+1, board, used, row, col-1, row_size, col_size);
+    }
+
+    if(!result && col<col_size-1 && !used[row*col_size+col+1]) {
+        result = dfs(word+1, board, used, row, col+1, row_size, col_size);
+    }
+
+    used[row*col_size +col] = false;
+    return result;
 }
