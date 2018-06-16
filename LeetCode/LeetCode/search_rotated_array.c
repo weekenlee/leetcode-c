2addc0c47567b218e52d26e4bd4ae2bcd1a92607
lwj1396@163.com <lwj1396@163.com>
2018-06-16 16:07:34 +0800

    81

--- LeetCode/LeetCode/search_rotated_array.c
+++ LeetCode/LeetCode/search_rotated_array.c
@@ -7,7 +7,7 @@ static int binary_search(int *nums, int size, int target)
     int low = -1;
     int high = size;
     while(low+1 < high) {
-        int mid = low + (high - lwo)/2;
+        int mid = low + (high - low)/2;
         if(target > nums[mid]) {
             low = mid;
         }else{
@@ -41,5 +41,23 @@ static bool search(int *nums, int numsSize, int target)
         return binary_search(nums, numsSize, target) >= 0;
     } else if(target >= nums[0]) {
         return binary_search(nums, i, target) >= 0;
+    } else if(target <= nums[numsSize-1]){
+        return binary_search(nums + i, numsSize -i, target)>=0;
+    }else {
+        return -1;
     }
+}
+
+int main(int argc, char **argv){
+    int i;
+    int target = atoi(argv[1]);
+    int size = argc -2;
+    int *nums = malloc(size * sizeof(int));
+
+    for(i = 0; i< argc-2; i++) {
+        nums[i] = atoi(argv[i+2]);
+    }
+
+    printf("%d\n", search(nums,size,target));
+    return 0;
 }
