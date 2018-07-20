f2457a231fff4f5f6eb8751264fb2370aaaae989
lwj1396@163.com <lwj1396@163.com>
2018-07-21 07:47:27 +0800

    sumgo

---
+++ LeetCode/LeetCode/combination_sum.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "sort"
+)
+
+func combinationSum(candidates []int , target int) [][]int {
+    sort.Ints(candidates)
+
+    res := [][]int{}
+    solution := []int{}
+    cs(candidates, solution, target, &res)
+
+    return res
+}
+
+func cs(candidates, solution []int , target int, result *[][]int) {
+    if target == 0 {
+        *result = append(*result, solution)
+    }
+
+    if(len(candidates) == 0 || target < candidates[0]) {
+        return
+    }
+
+    solution = solution[:len(solution):len(solution)]
+    cs(candidates, append(solution, candidates[0]),target-candidates[0], result)
+    cs(candidates[1:], solution, target, result)
+}
