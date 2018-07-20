b85f0db33d994765464a92ba7a0af4898bf7d106
lwj1396@163.com <lwj1396@163.com>
2018-07-21 07:39:39 +0800

    70go

---
+++ LeetCode/LeetCode/climb_stairs.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+)
+
+func climbStairs(n int) int {
+    if n < 2 {
+        return 1
+    }
+
+    rec := make([]int, n + 1)
+    rec[0], rec[1] = 1, 1
+
+    for i := 2; i <= n ; i++ {
+        rec[i] = rec[i-1] + rec[i-2]
+    }
+
+    return rec[n]
+}
+
+func main() {
+    fmt.Println(climbStairs(10))
+}
