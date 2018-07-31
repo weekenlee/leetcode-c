4c73782204fbe0dc75993bc983dfeab3c75d2cc5
lwj1396 <lwj1396@163.com>
2018-07-31 13:30:13 +0800

    go

---
+++ LeetCode/LeetCode/gray_code.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func grayCode(n int) []int {
+	return recur(n, 1, []int{0})
+}
+
+func recur(n, base int, nums []int) []int {
+	if n == 0 {
+		return nums
+	}
+	length := len(nums)
+	temp := make([]int, length)
+	for i:= range nums {
+		temp[length - i - 1] = nums[i] + base
+	}
+
+	return recur(n-1, base*2, append(nums, temp...))
+}
+
+func main() {
+	fmt.Println(grayCode(5))
+}
