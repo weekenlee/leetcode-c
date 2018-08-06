923a2b9226351af045faabdd5fd457f88993c44d
lwj1396 <lwj1396@163.com>
2018-08-06 19:15:40 +0800

    go

---
+++ LeetCode/LeetCode/insert_interval.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Interval struct {
+	Start int
+	End int
+}
+
+func insert(a []Interval, newItv Interval) []Interval {
+	lenA := len(a)
+
+	if lenA == 0 {
+		return []Interval{newItv}
+	}
+
+	if newItv.End < []
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
