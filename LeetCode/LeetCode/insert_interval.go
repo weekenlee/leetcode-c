a6c1b6d54556b21df9c70e009b87c0285698a228
lwj1396 <lwj1396@163.com>
2018-08-07 11:15:48 +0800

    insert_intervalgo

--- LeetCode/LeetCode/insert_interval.go
+++ LeetCode/LeetCode/insert_interval.go
@@ -14,9 +14,71 @@ func insert(a []Interval, newItv Interval) []Interval {
 		return []Interval{newItv}
 	}
 
-	if newItv.End < []
+	if newItv.End < newItv.Start {
+		return append(a, newItv)
+	}
+
+	res := make([]Interval, 0, len(a))
+	for i := range a {
+		if isOverlap(a[i], newItv) {
+			newItv = merge(a[i], newItv)
+
+			if i == lenA - 1 {
+				res = append(res, newItv)
+			}
+
+			continue
+		}
+
+		if a[i].End < newItv.Start {
+			res = append(res, a[i])
+			continue
+		}
+
+		if newItv.End < a[i].Start {
+			res = append(res, newItv)
+			res = append(res, a[i:]...)
+			break
+		}
+	}
+
+	return res
 }
 
+func isOverlap(a, b Interval) bool {
+	return (a.Start <= b.Start && b.Start <= a.End)
+}
+
+func merge(a, b Interval) Interval {
+	return Interval{
+		Start : min(a.Start, b.Start),
+		End:	max(a.End, b.End),
+	}
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a>b {
+		return a
+	}
+	return b
+}
+
+
 func main() {
-	fmt.Println("vim-go")
+	ni := Interval{3, 6}
+
+	an := []Interval{
+		{1,2},
+		{4,6},
+		{8,9},
+	}
+
+	fmt.Println(insert(an, ni))
 }
