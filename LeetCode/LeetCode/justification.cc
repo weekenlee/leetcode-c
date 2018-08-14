d39570120624566200398e0a2c4ed62fda438a3b
lwj1396 <lwj1396@163.com>
2018-08-14 09:01:42 +0800

    68go

---
+++ LeetCode/LeetCode/justification.cc
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using std::vector;
+using std::string;
+
+class Solution {
+    public:
+        vector<string> fullJustify(vector<string> &words, int maxWidth) {
+            vector<string> res;
+            const int n = words.size();
+            int begin = 0, len = 0;
+            for(int i = 0; i < n; ++i) {
+                if(len + words[i].size() + (i - begin) > maxWidth) {
+                    res.emplace_back(connect(words, maxWidth, begin, i, len, false));
+                    begin = i;
+                    len = 0;
+                }
+
+                len += words[i].size();
+            }
+
+            res.emplace_back(connect(words, maxWidth, begin, n, len, true));
+            return res;
+        }
+
+    private:
+        string connect(const vector<string>& words, int maxWidth, int begin,
+                int end, int len, bool is_last) {
+            string s;
+            int n = end - begin;
+            for(int i = 0; i<n; ++i) {
+                s += words[begin +i];
+                addSpaces(i, n-1, maxWidth - len, is_last, &s);
+            }
+            if(s.size() < maxWidth) {
+                s.append(maxWidth - s.size(), ' ');
+            }
+            return s;
+        }
+
+        void addSpaces(int i, int spaceCnt, int maxWidth, bool is_last, string *s) {
+            if (i < spaceCnt) {
+                int spaces = is_last ? 1 : maxWidth / spaceCnt + (i < maxWidth % spaceCnt);
+                s->append(spaces, ' ');
+            }
+
+        }
+};
+
+std::ostream& operator<<(std::ostream& o, std::vector<string> const& v) 
+{
+    for(auto i = v.begin(), end = v.end(); i != end; ++i) {
+        o << *i << std::endl; 
+    }
+    return o;
+}
+
+
+int main() 
+{
+    Solution s;
+    vector<string> words = {
+        "This", "is", "an", "exmaple", "of", "text", "justification."
+    };
+    std::cout<<s.fullJustify(words, 15)<<std::endl;
+    return 0;
+}
