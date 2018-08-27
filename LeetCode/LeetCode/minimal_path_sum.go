91e364b290ce95fecbebbd79b956e51caee5919d
lwj1396 <lwj1396@163.com>
2018-08-27 13:54:55 +0800

    64go

--- LeetCode/LeetCode/minimal_path_sum.go
+++ LeetCode/LeetCode/minimal_path_sum.go
@@ -6,6 +6,13 @@ func minPathSum(grid [][]int ) int {
 	m := len(grid)
 	n := len(grid[0])
 
+	for i := 0; i < m; i++ {
+		for j := 0;  j < n; j++ {
+			fmt.Printf("%v ", grid[i][j])
+		}
+		fmt.Printf("\n")
+	}
+
 	dp := make([][]int, m)
 
 	for i:= range dp {
@@ -13,23 +20,37 @@ func minPathSum(grid [][]int ) int {
 	}
 
 	dp[0][0] = grid[0][0]
-
-	for i := i; i < m ; i++ {
-		dp[i][0] = grid[i][0] + dp[i-1][0]
+	sum := dp[0][0]
+	for i := 1; i < m ; i++ {
+		sum += grid[i][0]
+		dp[i][0] = sum 
 	}
 
+	sum = dp[0][0]
 	for j := 1; j < n; j++ {
-		dp[0][j] = grid[0][j] + dp[0][j-1]
+		sum += grid[0][j]
+		dp[0][j] = sum 
 	}
 
-	for i :=1; i < m; i++ {
+	for i := 1; i < m; i++ {
 		for j := 1;  j < n; j++ {
 			dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
 		}
 	}
 
+	fmt.Println(dp)
 	return dp[m-1][n-1]
 }
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return a
+}
+
 func main() {
-	fmt.Println("vim-go")
+    //a.exe 131 151 421
+	grid := [][]int{[]int{1,3,1}, []int{1,5,1}, []int{4,2,1}}
+	fmt.Println(minPathSum(grid))
 }
