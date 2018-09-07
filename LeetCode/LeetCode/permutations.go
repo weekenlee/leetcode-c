e5aa61a839c4fe8ac38d79ce601766f6eb81c4e9
lwj1396 <lwj1396@163.com>
2018-09-07 14:05:05 +0800

    46go

---
+++ LeetCode/LeetCode/permutations.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func permute(nums []int) [][]int {
+	n := len(nums)
+	vector := make([]int, n)
+	taken := make([]bool, n)
+
+	var ans	[][]int
+
+	makePermutation(0, n , nums, vector, taken, &ans)
+	return ans
+}
+
+func makePermutation(cur, n int, nums, vector []int, taken []bool, 
+ans *[][]int ) {
+	if cur == n {
+		tmp := make([]int , n)
+		copy(tmp , vector)
+		*ans = append(*ans, tmp)
+		return 
+	}
+
+	for i := 0 ; i< n ;i++ {
+		if !taken[i] {
+			taken[i] = true
+			vector[cur] = nums[i]
+			makePermutation(cur + 1, n, nums, vector, taken, ans)
+			taken[i] = false
+		}
+	}
+}
+
+func main() {
+	fmt.Println(permute([]int{1,2}))
+	fmt.Println(permute([]int{1,2,3}))
+}
