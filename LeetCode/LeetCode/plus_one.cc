f697f96ce3bdceeaa0644059e4333b7bb384bfba
lwj1396 <lwj1396@163.com>
2018-09-10 10:48:06 +0800

    0cc

--- LeetCode/LeetCode/plus_one.cc
+++ LeetCode/LeetCode/plus_one.cc
@@ -2,6 +2,7 @@
 #include<vector>
 #include<ostream>
 #include<iterator>
+#include<algorithm>
 
 using namespace std;
 
@@ -22,6 +23,23 @@ class Solution {
         }
 };
 
+class Solution2 {
+    public:
+        vector<int> plusOne(vector<int>& digits) {
+            vector<int> result(digits.rbegin(), digits.rend());
+            int carry = 1;
+            for(auto &num : result) {
+                num += carry;
+                carry = num / 10;
+                num %= 10;
+            }
+            if (carry == 1) {
+                result.emplace_back(carry);
+            }
+            reverse(result.begin(), result.end());
+            return result;
+        }
+};
 
 int main(void) {
     Solution s;
@@ -30,6 +48,9 @@ int main(void) {
     for(auto i = r.begin(); i != r.end(); i++) {
        cout << *i <<endl; 
     }
+    vector<int> digits2{2,2,3};
+    Solution2 s2;
+    r = s2.plusOne(digits2) ;
     for (auto &i : r) {
         cout << i <<endl;
     }
