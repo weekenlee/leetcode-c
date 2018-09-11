720d07a15416fe0ced862409d8c56ab40cd54a8a
lwj1396 <lwj1396@163.com>
2018-09-11 11:37:44 +0800

    50cc

--- LeetCode/LeetCode/pow.cc
+++ LeetCode/LeetCode/pow.cc
@@ -18,9 +18,31 @@ class Solution {
         }
 };
 
+class Solution2 {
+    public:
+        double myPow(double x,int n) {
+            if (n < 0 && n != -n) {
+                return 1.0/myPow(x, -n);
+            } 
+
+            if (n == 0) {
+                return 1;
+            }
+
+            double v = myPow(x, n/2);
+            if (n % 2 == 0) {
+                return v*v;
+            }else {
+                return v * v * x;
+            }
+        }
+};
+
 int main(int argc, char* argv[]) 
 {
     Solution s;
     cout << s.myPow(2.0, 2) << endl;
+    Solution2  s2;
+    cout << s2.myPow(2.0, 2) << endl;
     return 0;
 }
