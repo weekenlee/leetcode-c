234c7642c80978718c6087d2370d3af6b23a9f00
lwj1396@163.com <lwj1396@163.com>
2018-09-15 11:22:25 +0800

    10py

---
+++ LeetCode/LeetCode/regular_expression.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isMatch(self, s, p):
+        if not p:
+            return not s
+
+        if len(p) == 1 or p[1] != '*':
+            if len(s) > 0 and (p[0] == s[0] or p[0] == '.'):
+                return self.isMatch(s[1:], p[1:])
+            else:
+                return False
+        else:
+            while len(s) > 0 and (p[0] == s[0] or p[0] == '.'):
+                if self.isMatch(s, p[2:]):
+                    return True
+                s = s[1:]
+                return self.isMatch(s, p[2:])
+
+if __name__=="__main__":
+    print(Solution().isMatch("abc","a*"))
+    print(Solution().isMatch("abc","a*bc"))
