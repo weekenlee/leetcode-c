476e39c9b4328fe7037343287bda37672ed5218d
lwj1396 <lwj1396@163.com>
2018-09-25 13:19:48 +0800

    48cc

--- LeetCode/LeetCode/rotate.cc
+++ LeetCode/LeetCode/rotate.cc
@@ -7,7 +7,7 @@ class Solution{
     public:
         void rotate(vector<vector<int>> &matrix) {
             const int n = matrix.size();
-            for(int i = i; i < n / 2; i++) {
+            for(int i = 0; i < n / 2; i++) {
                 for(int j = i; j < n -1 -i; ++j) {
                     const auto tmp = matrix[i][j];
                     matrix[i][j] = matrix[n-1-j][i];
@@ -18,3 +18,32 @@ class Solution{
             }
         }
 };
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+        fprintf(stderr, "Usage: ./test 3 3\n");
+    }
+
+    int i, j, count = 0;
+    int row_size = atoi(argv[1]);
+    int col_size = atoi(argv[2]);
+    vector<vector<int>> v{};
+    for (i = 0; i < row_size; i++) {
+        vector<int> vv{};
+        for (j = 0; j < col_size; j++) {
+            vv.push_back(++count);
+        }
+        v.push_back(vv);
+    }
+
+    Solution().rotate(v);
+    for (i = 0; i < col_size; i++) {
+        for (j = 0; j < row_size; j++) {
+            printf("%02d ",v[i][j]);
+        }
+        putchar('\n');
+    }
+
+    return 0;
+}
