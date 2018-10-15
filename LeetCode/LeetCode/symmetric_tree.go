f1a46e4bb868283fb536fca67e935d3d45ef4275
lwj1396@163.com <lwj1396@163.com>
2018-10-15 16:36:49 +0800

    101go

---
+++ LeetCode/LeetCode/symmetric_tree.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return recur(root.Left, root.Right)
+}
+
+func recur(left, right * TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val &&  recur(left.Left, right.Right) && recur(left.Right, right.Left)
+}
+
+
+func main() {
+	fmt.Println("vim-go")
+}
