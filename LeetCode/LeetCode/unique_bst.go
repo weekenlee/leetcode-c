aaa219367179c4dcda5ef356a8bff84395314dd1
lwj1396@163.com <lwj1396@163.com>
2018-10-16 18:41:31 +0800

    96go

---
+++ LeetCode/LeetCode/unique_bst.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func numTrees(n int) int {
+	if n == 0 {
+		return 1
+	}
+
+	if n == 1 || n == 2 {
+		return n
+	}
+
+	if n == 3 {
+		return 5
+	}
+
+	res := 0
+	for i:=1; i < n/2; i++ {
+		res += numTrees(i-1) * numTrees(n-i)
+	}
+
+	res *=2
+	if n %2 == 1 {
+		temp := numTrees(n/2)
+		res += temp * temp
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(numTrees(3))
+}
