4b05b2819899bec74ffa9c9f48959ea3deb39b86
lwj1396 <lwj1396@163.com>
2018-11-27 16:12:32 +0800

    115c

---
+++ LeetCode/LeetCode/distinct_subseq.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int numDistinct(char *s, char *t) {
+    int s_len = strlen(s);
+    int t_len = strlen(t);
+
+    if(t_len == 0 || s_len < t_len) {
+        return 0;
+    }
+
+    int start = 0;
+    while(start < s_len && s[start] != t[0]) {
+        start++;
+    }
+    if(start == s_len) {
+        return 0;
+    }
+
+    int i, j;
+    int *table = malloc((s_len - start) * t_len * sizeof(int));
+    int **dp = malloc((s_len - start) * sizeof(int *));
+    for(i = 0; i < s_len - start; i++) {
+        dp[i] = &table[i * t_len];
+    }
+
+    dp[0][0] = 1;
+    for(i =1; i < t_len; i++) {
+        dp[0][i] = 0;
+    }
+
+    for(i = 1; i < s_len - start; i++) {
+        dp[i][0] = dp[i-1][0];
+        if(s[start + i] == t[0]) {
+            dp[i][0]++;
+        }
+    }
+
+}
