b35ef8bf57c9646967b9b7e14618e35720aa5e6f
lwj1396 <lwj1396@163.com>
2019-01-07 09:03:11 +0800

    132cc

--- LeetCode/LeetCode/clone_graph.cc
+++ LeetCode/LeetCode/clone_graph.cc
@@ -6,7 +6,7 @@ using std::unordered_map;
 struct UndirectedGraphNode {
     int label;
     vector<UndirectedGraphNode *> neighbors;
-    UndirectedGraph(int x) : label(x) {}
+    UndirectedGraphNode(int x) : label(x) {}
 };
 
 class Solution {
@@ -37,10 +37,65 @@ class Solution {
 
 void printUndirectedGraph(UndirectedGraphNode *node) {
     std::ostringstream out;
-    std::unordered_map<int,std::unordered_set<int>> map{0,std::unordered_set<int>(){}};
+    std::unordered_map<int,std::unordered_set<int>> map{{0,std::unordered_set<int>()}};
     std::deque<UndirectedGraphNode*> cache;
     cache.push_back(node);
     do{
-        std::sort()
-    }
+        std::sort(cache.begin(), cache.end(), [](UndirectedGraphNode* n1, UndirectedGraphNode *n2) {
+            return n1->label < n2->label;
+        });
+        UndirectedGraphNode *node = cache.front();
+        cache.pop_front();
+        out << node -> label;
+        auto found = map.find(node->label);
+        for(UndirectedGraphNode* neighbor : node->neighbors) {
+            if(map.find(neighbor->label) == map.end()) {
+                cache.push_back(neighbor);
+            }
+            if(found != map.end()) {
+                std::unordered_set<int> &exist = found->second;
+                if(exist.find(neighbor->label) == exist.end()) {
+                    out << "," << neighbor->label;
+                }
+            }
+            map[neighbor->label].emplace(node->label);
+        }
+        out << "#";
+    } while(!cache.empty());
+
+    std::string out_str = out.str();
+    std::cout << out_str.substr(0, out_str.size()-1) <<std::endl;
+}
+
+int main() {
+        Solution s;
+            
+        UndirectedGraphNode node0(0);
+        UndirectedGraphNode node1(1);
+        UndirectedGraphNode node2(2);
+        UndirectedGraphNode node3(3);
+        UndirectedGraphNode node4(4);
+        UndirectedGraphNode node5(5);
+        node0.neighbors.push_back(&node1);
+        node0.neighbors.push_back(&node5);
+        node1.neighbors.push_back(&node0);
+        node1.neighbors.push_back(&node2);
+        node1.neighbors.push_back(&node5);
+        node2.neighbors.push_back(&node1);
+        node2.neighbors.push_back(&node3);
+        node3.neighbors.push_back(&node2);
+        node3.neighbors.push_back(&node4);
+        node3.neighbors.push_back(&node4);
+        node4.neighbors.push_back(&node3);
+        node4.neighbors.push_back(&node3);
+        node4.neighbors.push_back(&node5);
+        node4.neighbors.push_back(&node5);
+        node5.neighbors.push_back(&node0);
+        node5.neighbors.push_back(&node1);
+        node5.neighbors.push_back(&node4);
+        node5.neighbors.push_back(&node4);
+        
+        printUndirectedGraph(&node0);
+        UndirectedGraphNode *node = s.cloneGraph(&node0);
+        printUndirectedGraph(node);
 }
