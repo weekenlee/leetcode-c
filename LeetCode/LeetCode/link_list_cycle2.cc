674852fb18b3d9214df946e31a6f894ae9059ba9
lwj1396 <lwj1396@163.com>
2019-01-21 15:45:25 +0800

    142cc

---
+++ LeetCode/LeetCode/link_list_cycle2.cc
@@ -0,0 +1,28 @@
+#include <cstddef>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+    public:
+        ListNode *deleteCycle(ListNode *head) {
+            ListNode *slow = head,  *fast = head;
+
+            while(fast && fast->next) {
+                slow = slow->next, fast = fast->next->next;
+                if(slow == fast) {
+                    slow = head;
+                    while (slow != fast) {
+                        slow = slow->next, fast = fast->next;
+                    }
+                    return slow;
+                }
+            }
+            return nullptr;
+        }
+};
+
+
