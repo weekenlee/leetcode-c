43498162fc0002cf0fb5351cf7210ef8844adfeb
lwj1396@163.com <lwj1396@163.com>
2019-02-14 13:47:00 +0800

    146go

--- LeetCode/LeetCode/lru_cache.go
+++ LeetCode/LeetCode/lru_cache.go
@@ -4,8 +4,8 @@ import "fmt"
 
 
 //双向链表节点
-type doublyLinkNode struct {
-	prev, next *doublyLinkNode
+type doublyLinkedNode struct {
+	prev, next *doublyLinkedNode
 	key, val int
 }
 
@@ -34,8 +34,74 @@ func (c *LRUCache) Get(key int) int {
 	return -1
 }
 
+// Put 放入数据
+func (c *LRUCache ) Put(key int, value int) {
+	node, ok := c.nodes[key]
 
+	if ok {
+		node.val = value
+		c.moveToFirst(node)
+	}else {
+		if c.len == c.cap {
+			delete(c.nodes, c.last.key)
+			c.removeLast()
+		} else {
+			c.len++
+		}
+
+		node = &doublyLinkedNode {
+			key : key,
+			val : value,
+		}
+
+		c.nodes[key] = node
+		c.insertToFirst(node)
+	}
+}
+
+func (c *LRUCache) removeLast() {
+	if c.last.prev != nil {
+		c.last.prev.next = nil
+	} else {
+		c.first = nil
+	}
+
+	c.last = c.last.prev
+}
+
+func (c *LRUCache) moveToFirst(node *doublyLinkedNode) {
+	switch node {
+	case c.first:
+		return
+	case c.last:
+		c.removeLast()
+	default:
+		node.prev.next = node.next
+		node.next.prev = node.prev
+	}
+
+	c.insertToFirst(node)
+}
+
+
+func (c *LRUCache) insertToFirst(node *doublyLinkedNode) {
+	if c.last == nil {
+		c.last = node
+	}else {
+		node.next = c.first
+		c.first.prev = node
+	}
+
+	c.first = node
+}
 
 func main() {
-	fmt.Println("vim-go")
+	cache := Constructor(3)
+	cache.Put(0,0)
+	cache.Put(1,1)
+	cache.Put(2,2)
+
+	fmt.Println(cache.Get(0))
+	fmt.Println(cache.Get(1))
+	fmt.Println(cache.Get(2))
 }
