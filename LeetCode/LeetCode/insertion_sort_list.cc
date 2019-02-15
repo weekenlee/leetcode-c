4df799791d28aa788aab15bf93b4700d7e318c3c
lwj1396@163.com <lwj1396@163.com>
2019-02-15 13:53:44 +0800

    147cc

---
+++ LeetCode/LeetCode/insertion_sort_list.cc
@@ -0,0 +1,37 @@
+#include<cstddef>
+#include<limits>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+    public:
+        ListNode *insertionSortList(ListNode *head) {
+            ListNode dummy{std::numeric_limits<int>::min()};
+
+            auto curr = head;
+            ListNode *position = nullptr;
+
+            while(curr) {
+                position = findInsertPosition(&dummy, curr->val);
+                ListNode *tmp = curr->next;
+                curr->next = position->next;
+                position->next = curr;
+                curr = tmp;
+            }
+
+            return dummy.next;
+        }
+
+        ListNode *findInsertPosition(ListNode *head, int x) {
+            ListNode *prev = nullptr;
+            for(auto curr = head; curr && curr->val <= x; prev = curr,
+                    curr = curr->next) {
+            }
+
+            return prev;
+        }
+};
