e97f2b5a4b03ae7924c4160610316303853de715
lwj1396 <lwj1396@163.com>
2019-03-07 14:18:14 +0800

    167cc

---
+++ LeetCode/LeetCode/two-sum-ii-input-array-is-sorted.cc
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& numbers, int target) {
+            int left = 0, right = numbers.size() - 1;
+
+            while(left != right) {
+                const auto sum = numbers[left] + numbers[right];
+                if(sum > target) {
+                    --right;
+                }else if(sum < target) {
+                    ++left;
+                }else {
+                    return {left + 1, right + 1};
+                }
+            }
+
+            return {0, 0};
+        }
+};
