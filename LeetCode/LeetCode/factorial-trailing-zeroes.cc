381a77a0ac5df852698e9326d280dc90ca0f6793
lwj1396 <lwj1396@163.com>
2019-03-18 08:51:32 +0800

    172cc

---
+++ LeetCode/LeetCode/factorial-trailing-zeroes.cc
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+        int trailingZeroes(int n) {
+            int number = 0;
+            while (n > 0) {
+                number += n / 5;
+                n /= 5;
+            }
+            return number;
+        }
+};
