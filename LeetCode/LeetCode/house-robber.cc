7d1e7203543196c662f70ee1b5ef08f37c5bb2c9
lwj1396 <lwj1396@163.com>
2019-03-25 10:57:59 +0800

    198

---
+++ LeetCode/LeetCode/house-robber.cc
@@ -0,0 +1,19 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+    public:
+        int rob(vector<int> &nums) {
+            int last = 0  , result = 0;
+            for(const auto& i : nums) {
+                auto tmp = result;
+                result = max(last + i, result);
+                last = tmp;
+            }
+            return result;
+        }
+};
+
+int main() {
+    
+}
