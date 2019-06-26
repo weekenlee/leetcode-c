9989639b50cbce5223b69e2eef9a47a761f45b85
lwj1396 <lwj1396@163.com>
2019-06-26 16:16:35 +0800

    217

---
+++ LeetCode/LeetCode/contains-duplicate.cc
@@ -0,0 +1,33 @@
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+    public:
+        bool containsDuplicate(vector<int>& nums) {
+            unordered_set<int> nums_set(nums.begin(), nums.end());
+            return nums_set.size() != nums.size();
+        }
+};
+
+
+class Solution2 {
+    public:
+        bool containsDuplicate(vector<int> &nums) {
+            sort(nums.begin(), nums.end());
+            return unique(nums.begin(), nums.end()) != nums.end();
+        }
+};
+
+#include<iostream>
+
+int main()
+{
+    Solution s;
+    Solution2 s2;
+    vector<int> v {1,2,3,1};
+    cout << s.containsDuplicate(v) <<endl;
+    cout << s2.containsDuplicate(v) <<endl;
+}
