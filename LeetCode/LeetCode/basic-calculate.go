d292113714d313b5e329238692a29d53c2ed7e95
lwj1396@163.com <lwj1396@163.com>
2019-08-15 21:56:59 +0800

    224go

---
+++ LeetCode/LeetCode/basic-calculate.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func calculate(s string) int {
+	res := 0
+	stack := make([]int, 0, len(s))
+	sign := 1
+	num := 0
+
+	for i := 0; i < len(s);  i++ {
+		switch s[i] {
+		case '1','2','3','4','5','6','7','8','9','0':
+			num = 0
+			for ; i < len(s) && s[i] >= '0' && s[i] <= '9'; i++ {
+				num = 10 * num + int( s[i]-'0' )
+			}
+
+			res += sign * num
+			i--
+		case '+':
+			sign = 1
+		case '-':
+			sign = -1
+		case '(':
+			stack = append(stack, res, sign)
+			res = 0
+			sign = 1
+		case ')':
+			sign = stack[len(stack) - 1]
+			temp := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+			res = sign*res + temp
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(calculate("1+1+2"))
+	fmt.Println(calculate("1+1-2"))
+}
