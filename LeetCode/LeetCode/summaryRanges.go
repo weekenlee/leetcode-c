eec7fc7f13417b16568661e12c5503fc26fada59
lwj1396@163.com <lwj1396@163.com>
2019-09-16 22:42:22 +0800

    228go

---
+++ LeetCode/LeetCode/summaryRanges.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func summaryRanges(a []int) []string {
+	res := []string{}
+
+	l := len(a)
+	if l == 0 {
+		return res
+	}
+
+	begin := a[0]
+	str := ""
+
+	for i := 0; i < l; i++ {
+		if i == l - 1 || a[i] + 1 != a[i+1] {
+			if a[i] == begin {
+				str = fmt.Sprintf("%d", begin)
+			}else {
+				str = fmt.Sprintf("%d->%d", begin, a[i])
+			}
+
+			if i + 1 < l {
+				begin= a[i+1]
+			}
+
+			res = append(res, str)
+		}
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(summaryRanges([]int{1,2,3,4,8,9,11}))
+}
