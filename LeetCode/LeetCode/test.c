a186280c3472f64abecbc77c21f840c05a2740cc
lwj1396@163.com <lwj1396@163.com>
2022-01-23 21:19:58 +0800

    printf

--- LeetCode/LeetCode/test.c
+++ LeetCode/LeetCode/test.c
@@ -1,4 +1,7 @@
 #include<inttypes.h>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
 
 void vga_putchar(int c, int color, int x, int y)
 {
@@ -12,9 +15,29 @@ int main()
 {
 //vga_putchar('a', 0x07, 0, 0);
 
-  char *src = (char *)0xB8000000L;
-  *src = 'M';
-  src += 2;
-  *src = 'D';
+//  char *src = (char *)0xB8000000L;
+//  *src = 'M';
+//  src += 2;
+//  *src = 'D';
+
+  char *src = "helloworld";
+  printf("%s\n", src);
+
+
+  //printf("%c",*src);
+  //printf("%c",*(src+1));
+  //printf("%c",*(src+2));
+  //printf("\n");
+
+  printf("%ld\n",strlen(src));
+  char *des = malloc(strlen(src) + 1);
+  char *p = des;
+  while(*src != '\0') {
+      printf("%c",*src);
+      *p++ = *src++;
+  }
+  printf("\n");
+
+  printf("%s\n", des);
   return 0;
 }
